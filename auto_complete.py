adb784bfe5f51e731d245be1a745a0f562c73843
Purpl3 <remezdevid2000@gmail.com>
2023-02-03 18:58:38 +0200

    update?

--- auto_complete.py
+++ auto_complete.py
@@ -1,10 +1,15 @@
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
+from rich.console import Console
 from selenium import webdriver
 from time import sleep as wait
+from rich import print
 import random
-def get_all(test_start_url: str, answers: dict, name: str, enable_random_delay: bool):
+
+console = Console()#Rich console
+
+def get_all(test_start_url: str, answers: dict, name: str, enable_random_delay: bool,errors_count):
+
+    err_count = int(errors_count)
 
     driver = webdriver.Firefox()
 
@@ -18,38 +23,52 @@ def get_all(test_start_url: str, answers: dict, name: str, enable_random_delay: 
     wait(3)#Wait for page load
 
     questions_count = driver.find_element(By.XPATH,'/html/body/div[1]/div/nav/div/div/div[1]/div/div/span[2]').text
+    with console.status("[bold cyan]📝 Completing test...") as status:
+        for x in range(int(questions_count)):
 
-    for x in range(int(questions_count)):
-
-        if enable_random_delay:
-            wait(random.randint(5,20))
-         
-        elif not enable_random_delay:
-            wait(5)
+            if enable_random_delay:
+                wait(random.randint(5,20))
             
+            elif not enable_random_delay:
+                wait(5)
+                
+            status.stop()
 
-        question = driver.find_element(By.XPATH,'/html/body/div[1]/div/div/div/div/div/div[2]/div[1]/div/div[2]/div').text#Вопрос
-        print('Current question: '+question)
-        id_question = str(driver.find_element(By.XPATH, '/html/body/div[1]/div/nav/div/div/div[1]/div/div/span[1]').text)#Айди вопроса
-        print('ID of question: '+id_question)
+            question = driver.find_element(By.XPATH,'/html/body/div[1]/div/div/div/div/div/div[2]/div[1]/div/div[2]/div').text#Вопрос
+            print('[green]Current question: '+question)
+            id_question = str(driver.find_element(By.XPATH, '/html/body/div[1]/div/nav/div/div/div[1]/div/div/span[1]').text)#Айди вопроса
+            print('[green]ID of question: '+id_question)
+            
+            status.start()
+
+            quiz_buttons = {}
+
+            for button_index,button_letter in zip(range(1,4+1),['а','б','в','г']):    
+                quiz_buttons[button_letter] = driver.find_element(By.XPATH, f'/html/body/div[1]/div/div/div/div/div/div[2]/div[2]/div/div[{button_index}]/div/div')
+
+            if len(answers[id_question][1]) <= 1:#if answers is bigger than 1
+                print(err_count)
+                if err_count == 0:
+                    quiz_buttons[answers[id_question][1][0]].click()
+                
+                elif err_count >= 1:
+                    random_button_to_error = random.choice(list(quiz_buttons.keys()))
+
+                    if random_button_to_error!=answers[id_question][1][0]:
+                        quiz_buttons[random_button_to_error].click()
+
+                    err_count-=1
+                    
+            if len(answers[id_question][1]) > 1:
+                for temp in answers[id_question][1]:
+                    quiz_buttons[temp].click()#click on sucess buttons
+
+                driver.find_element(By.XPATH,'/html/body/div[1]/div/div/div/div/div/div[2]/div[3]/a').click()#Click on button Sumbit
         
-        quiz_buttons = {}
-
-        for button_index,button_letter in zip(range(1,4+1),['а','б','в','г']):    
-            quiz_buttons[button_letter] = driver.find_element(By.XPATH, f'/html/body/div[1]/div/div/div/div/div/div[2]/div[2]/div/div[{button_index}]/div/div')
-
-        if len(answers[id_question][1]) <= 1:#if answers is bigger than 1
-            quiz_buttons[answers[id_question][1][0]].click()
-
-        elif len(answers[id_question][1]) > 1:
-            for temp in answers[id_question][1]:
-                quiz_buttons[temp].click()
-
-            driver.find_element(By.XPATH,'/html/body/div[1]/div/div/div/div/div/div[2]/div[3]/a').click()
-
+    
     wait(5)
 
 
         
-    if input('Close window [y,n]: ').lower() == 'y':
+    if console.input('[yellow]Close window (y,n): ').lower() == 'y':
         driver.close()
--- main.py
+++ main.py
@@ -1,21 +1,22 @@
 from rich.console import Console
-from download import download
 from bs4 import BeautifulSoup
+from auto_complete import *
 from rich import print
 import googlesearch
 import requests
 import os, sys
-from auto_complete import *
-try:os.remove('geckodriver.log')
-except FileNotFoundError:pass
 
 os.chdir(sys.path[0])
 
+if not os.path.isfile('config.py'):
+    with open('config.py','w') as cfg:
+        cfg.write("PHPSESSID = 'ENTER YOUR PHP SESSION ID HERE!'")
+    print('[purple]Check root dir and fill PHPSESSID var in config.py')
+    quit()
+
+from config import *
+
 console = Console()#Rich console
-
-
-if not os.path.isfile('./geckodriver'):
-    download('https://raw.githubusercontent.com/purpl3-yt/trash-bin/main/files_for_my_programs/geckodriver','./geckodriver',replace=True)
 
 print('''[red]
   _   _                       _       _____ _           _           
@@ -27,21 +28,26 @@ print('''[red]
 
 do_not_search = False
 
-question = console.input('[yellow]Enter a random question from test or link: [/yellow]')
+question = console.input('[yellow]Enter a random question from test or link: ')
 
-if question.lower().startswith('https'):
+if question.lower().startswith('http'):
     do_not_search = True
 
+find = False
 if not do_not_search:
-    print('Search test url...')
-    
-    for result in googlesearch.search(question+' inurl:naurok',lang='ua',num_results=30):
-        if 'naurok' in result and '/test' in result:
-            if input(f'Found test url: {result}\nSkip [y,n]: ').lower() == 'n':
-                test_url = result
-                break
-            else:
-                pass
+    with console.status("[bold cyan]🔎 Search test url...") as status:
+        while not find:
+            for result in googlesearch.search(question,lang='ua',num_results=30):
+                if 'naurok' in result and '/test' in result:
+                    status.stop()
+                    if console.input(f'[green]Found test url: [/green][blue]{result}[/blue]\n[yellow]Skip (y,n): [/yellow]').lower() == 'n':
+                        test_url = result
+                        find=True
+                        break
+                    else:
+                        status.start()
+                        pass
+                        
         
 if do_not_search:
     test_url = question
@@ -64,8 +70,7 @@ for a in soup.find_all('a',class_='test-action-button', href=True):
 test = requests.get(print_url,
 headers={
         'User-Agent':'Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/109.0',
-
-    },cookies={'PHPSESSID':'ENTER_YOUR_PHPSESSID_HERE!!!'})
+    },cookies={'PHPSESSID':PHPSESSID})
     
 soup = BeautifulSoup(test.content, 'lxml')
 
@@ -90,14 +95,17 @@ for div,question_num,key in zip(soup.find_all('div',class_='col-md-11 no-padding
         str(div.find('p').text),#Question
         [x for x in str(key[str(key).find('.')+1:str(key).find('(')]).split(' ') if x!=''])#Answers
 
+print('[purple]Answers: ')
 print(questions_dict)
-print('Print url: '+print_url)
+print('[green]Print url: [/green][blue]'+print_url)
 
-if input('Auto complete [y,n]: ').lower() == 'y':
+if console.input('[yellow]Auto complete (y,n): ').lower() == 'y':
     conv_boolean = {'y':True,'n':False}
-    name = input('Enter name: ')
-    enable_random_delay = input('Enable random delay [y,n]: ')
-    get_all(test_start_url,questions_dict,name,conv_boolean[enable_random_delay.lower()])
+    name = console.input('[yellow]Enter name: ')
+    enable_random_delay = console.input('[yellow]Enable random delay (y,n): ')
+    count_of_errors = console.input('[yellow]Count of errors (0 for disable): ')
+
+    get_all(test_start_url,questions_dict,name,conv_boolean[enable_random_delay.lower()],count_of_errors)
 
 else:
     quit()
